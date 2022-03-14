743cacc6aaceb27e748dd4bdc1bffa2a51a959c5
j__c <johnmichaelcollins@protonmail.com>
2022-03-13 22:02:12 -0400

    lua/nvterm_config.lua - added nvterm_config

---
+++ lua/nvterm_config.lua
@@ -0,0 +1,11 @@
+-- [[ nvterm config ]]
+
+
+-----------------------[[
+--       keymaps       --
+-----------------------]]
+
+
+vim.keymap.set('n', '<leader>5', ':Nvterm<cr>', {noremap=true, silent=false})
+vim.keymap.set('n', '<leader>4', ':Behave<cr>', {noremap=true, silent=false})
+vim.keymap.set('n', '<leader>3', ':Unittest<cr>', {noremap=true, silent=false})
