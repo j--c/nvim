6c2b14673bd8b8d637857091933ac7989ce1a62e
j--c <johnmichaelcollins@protonmail.com>
2023-11-18 09:31:27 -0500

    removed all of the keymaps that were commented out

--- after/plugin/nvterm_config.lua
+++ after/plugin/nvterm_config.lua
@@ -1,17 +1,3 @@
---[[ 
-    local command_tbl = {}
-    command_tbl['start flaskapp'] = '"python3 start_flaskapp.py\\n")'
-    command_tbl['python unittest'] = '"python3 -m unittest -v\\n")'
-
-    require('nvterm.init'.setup(command_tbl))
-
-    vim.keymap.set('n', '<leader>2', ':Nvterm<cr>', {noremap=true, silent=false})
-    vim.keymap.set('n', '<leader>3', ':NvtermVSplit<cr>', {noremap=true, silent=false})
-    vim.keymap.set('n', '<leader>4', ':NvtermHSplit<cr>', {noremap=true, silent=false})
-    vim.keymap.set('n', '<leader>5', ':Unittest<cr>', {noremap=true, silent=false})
-    vim.keymap.set('n', '<leader>6', ':FlaskApp<cr>', {noremap=true, silent=false})
---]]
-
 local nvterm = require('nvterm')
 require 'nvterm'.add_cmd(6, '"python3 start_flaskapp.py')
 require 'nvterm'.add_cmd(5, '"python3 -m unittest -v')
