00bb0a4ec30cee17b53d9731d2249b99f202edee
j--c <johnmichaelcollins@protonmail.com>
2023-11-18 21:57:37 -0500

    removed double quote from the terminal commands

--- after/plugin/nvterm_config.lua
+++ after/plugin/nvterm_config.lua
@@ -1,8 +1,9 @@
 local nvterm = require('nvterm')
 
 require 'nvterm'.setup({
-    [5] = '"python3 -m unittest -v',
-    [6] = '"python3 start_flaskapp.py'
+    [5] = 'python3 -m unittest -v',
+    [6] = 'python3 start_flaskapp.py',
+    [7] = 'git push origin main'
 })
 
 vim.keymap.set('n', '<leader>2', function() 
@@ -23,4 +24,8 @@ end)
 
 vim.keymap.set('n', '<leader>6', function() 
     require 'nvterm'.run('h', 6)
+end)
+
+vim.keymap.set('n', '<leader>7', function() 
+    require 'nvterm'.run('h', 7)
 end)
