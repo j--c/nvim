23f69d6c2436baa8a2e8b4767f515d4ffef37f79
j--c <johnmichaelcollins@protonmail.com>
2023-11-20 18:57:47 -0500

    changed netrw open from Ex to Vex

--- lua/global_keymaps.lua
+++ lua/global_keymaps.lua
@@ -2,7 +2,7 @@
 vim.g.mapleader = ","
 
 -- netrw
-vim.keymap.set('n', '<leader>1', vim.cmd.Ex)
+vim.keymap.set('n', '<leader>1', vim.cmd.Vex)
 
 -- hidden characters
 vim.keymap.set('n', '<leader>0', ':set list!<cr>', {noremap=true, silent=false})
