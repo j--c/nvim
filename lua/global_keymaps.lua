29a40e09cc451246eb16167b2684b04279f23748
j--c <johnmichaelcollins@protonmail.com>
2023-11-20 18:00:54 -0500

    added keymaps for splits

--- lua/global_keymaps.lua
+++ lua/global_keymaps.lua
@@ -15,6 +15,10 @@ vim.keymap.set('n', '<leader>d', ':bd<cr>', {noremap=true, silent=false})
 vim.keymap.set('n', '<leader>w', '<c-w>w', {noremap=true, silent=false})
 vim.keymap.set('n', '<leader>c', '<c-w>c', {noremap=true, silent=false})
 
+-- splits
+vim.keymap.set('n', '<leader>v', ':vsplit<cr>', {noremap=true, silent=false})
+vim.keymap.set('n', '<leader>h', ':sp<cr>', {noremap=true, silent=false})
+
 -- movement
 vim.keymap.set('n', '<right>', '<nop>', {noremap=true, silent=false})
 vim.keymap.set('n', '<left>', '<nop>', {noremap=true, silent=false})
