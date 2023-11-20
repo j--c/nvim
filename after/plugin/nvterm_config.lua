7b2cad939aed6d11595f28888f657f2dbcb72c4e
j--c <johnmichaelcollins@protonmail.com>
2023-11-20 18:03:56 -0500

    added new commands to nvterm setup

--- after/plugin/nvterm_config.lua
+++ after/plugin/nvterm_config.lua
@@ -1,31 +1,29 @@
-local nvterm = require('nvterm')
-
 require 'nvterm'.setup({
     [5] = 'python3 -m unittest -v',
-    [6] = 'python3 start_flaskapp.py',
+    [6] = 'python3 app.py',
     [7] = 'git push origin main'
 })
 
-vim.keymap.set('n', '<leader>2', function() 
+vim.keymap.set('n', '<leader>2', function()
     require 'nvterm'.run('', nil)
 end)
 
-vim.keymap.set('n', '<leader>3', function() 
+vim.keymap.set('n', '<leader>3', function()
     require 'nvterm'.run('v', nil)
 end)
 
-vim.keymap.set('n', '<leader>4', function() 
+vim.keymap.set('n', '<leader>4', function()
     require 'nvterm'.run('h', nil)
 end)
 
-vim.keymap.set('n', '<leader>5', function() 
+vim.keymap.set('n', '<leader>5', function()
     require 'nvterm'.run('h', 5)
 end)
 
-vim.keymap.set('n', '<leader>6', function() 
+vim.keymap.set('n', '<leader>6', function()
     require 'nvterm'.run('h', 6)
 end)
 
-vim.keymap.set('n', '<leader>7', function() 
+vim.keymap.set('n', '<leader>7', function()
     require 'nvterm'.run('h', 7)
 end)
