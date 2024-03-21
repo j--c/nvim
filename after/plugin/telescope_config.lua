296a37c5370757a0fc1728446d4d680c2afca3b0
j--c <johnmichaelcollins@protonmail.com>
2024-03-21 15:18:02 +0000

    lsp_references and treesitter

--- after/plugin/telescope_config.lua
+++ after/plugin/telescope_config.lua
@@ -3,6 +3,8 @@ vim.keymap.set('n', '<leader>?', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>1', builtin.find_files, {})
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>gt', builtin.git_files, {})
+vim.keymap.set('n', '<leader>rf', builtin.lsp_references, {})
+vim.keymap.set('n', '<leader>tr', builtin.treesitter, {})
 vim.keymap.set('n', '<leader>gr', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
