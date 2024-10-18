32c7ce1e19ed4579ba8fb96c89f770ea9fe8783c
j--c <johnmichaelcollins@protonmail.com>
2024-10-17 21:58:23 -0400

    updated nvcd_config to account for new changes in nvcd

--- after/plugin/nvcd_config.lua
+++ after/plugin/nvcd_config.lua
@@ -1,7 +1,24 @@
 vim.keymap.set('n', '<leader>9', function()
-    require 'nvcd'.change_working_dir('~/.config/nvim')
+    require 'nvcd'.change_working_dir(
+        {
+            cur_wd_path = vim.fn['getcwd'](),
+            new_wd_path = '~/.config/nvim',
+            relative = false
+        }
+    )
 end)
 
 vim.keymap.set('n', '<leader>n', function()
-    require 'nvcd'.change_working_dir('~/gtd/note')
+    local has_prev = require 'nvcd'.has_previous_wd()
+    local usr_path = ''
+    if has_prev == false then
+        usr_path = vim.fn.input('Dir > ')
+    end
+    require 'nvcd'.change_working_dir(
+        {
+            cur_wd_path = vim.fn['getcwd'](),
+            new_wd_path = usr_path,
+            relative = true
+        }
+    )
 end)
