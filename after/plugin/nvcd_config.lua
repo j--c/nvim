d30dc6528622c98d4e4afcdd629cb5e1a6a63965
j--c <johnmichaelcollins@protonmail.com>
2024-10-19 16:07:06 -0400

    updated nvcd_config to work with the the redesigned nvcd plugin

--- after/plugin/nvcd_config.lua
+++ after/plugin/nvcd_config.lua
@@ -1,24 +1,13 @@
-vim.keymap.set('n', '<leader>9', function()
-    require 'nvcd'.change_working_dir(
-        {
-            cur_wd_path = vim.fn['getcwd'](),
-            new_wd_path = '~/.config/nvim',
-            relative = false
-        }
-    )
-end)
+vim.keymap.set(
+    'n',
+    '<leader>m',
+    ':SwapNvimConfig<cr>',
+    {noremap=true, silent=false}
+)
 
-vim.keymap.set('n', '<leader>n', function()
-    local has_prev = require 'nvcd'.has_previous_wd()
-    local usr_path = ''
-    if has_prev == false then
-        usr_path = vim.fn.input('Dir > ')
-    end
-    require 'nvcd'.change_working_dir(
-        {
-            cur_wd_path = vim.fn['getcwd'](),
-            new_wd_path = usr_path,
-            relative = true
-        }
-    )
-end)
+vim.keymap.set(
+    'n',
+    '<leader>n',
+    ':SwapNvimCustom<cr>',
+    {noremap=true, silent=false}
+)
