b3d990e4f27058037e50c99994d8ad675cf68dd6
j--c <johnmichaelcollins@protonmail.com>
2024-10-19 13:26:41 -0400

    changed colorschme so it is automatic

--- after/plugin/global_colorscheme.lua
+++ after/plugin/global_colorscheme.lua
@@ -1,85 +1,118 @@
-local M = {}
+require('lualine').setup {
+  options = {
+    icons_enabled = false,
+    component_separators = {''},
+    section_separators = {''},
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = true,
+    globalstatus = false,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    }
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'filename'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {}
+}
 
-M.load_material = function()
-    require('material').setup({
-        contrast = {
-            terminal = false, -- Enable contrast for the built-in terminal
-            sidebars = false, -- Enable contrast for sidebar-like windows ( for example Nvim-Tree )
-            floating_windows = false, -- Enable contrast for floating windows
-            cursor_line = false, -- Enable darker background for the cursor line
-            lsp_virtual_text = false, -- Enable contrasted background for lsp virtual text
-            non_current_windows = false, -- Enable contrasted background for non-current windows
-            filetypes = {}, -- Specify which filetypes get the contrasted (darker) background
-        },
+require('material').setup {
+    contrast = {
+        terminal = false, -- Enable contrast for the built-in terminal
+        sidebars = false, -- Enable contrast for sidebar-like windows ( for example Nvim-Tree )
+        floating_windows = false, -- Enable contrast for floating windows
+        cursor_line = false, -- Enable darker background for the cursor line
+        lsp_virtual_text = false, -- Enable contrasted background for lsp virtual text
+        non_current_windows = false, -- Enable contrasted background for non-current windows
+        filetypes = {}, -- Specify which filetypes get the contrasted (darker) background
+    },
 
-        styles = { -- Give comments style such as bold, italic, underline etc.
-            comments = { --[[ italic = true ]] },
-            strings = { --[[ bold = true ]] },
-            keywords = { --[[ underline = true ]] },
-            functions = { --[[ bold = true, undercurl = true ]] },
-            variables = {},
-            operators = {},
-            types = {},
-        },
+    styles = { -- Give comments style such as bold, italic, underline etc.
+        comments = { --[[ italic = true ]] },
+        strings = { --[[ bold = true ]] },
+        keywords = { --[[ underline = true ]] },
+        functions = { --[[ bold = true, undercurl = true ]] },
+        variables = {},
+        operators = {},
+        types = {},
+    },
 
-        plugins = { -- Uncomment the plugins that you use to highlight them
-            -- Available plugins:
-            -- "coc",
-            -- "colorful-winsep",
-            -- "dap",
-            -- "dashboard",
-            -- "eyeliner",
-            -- "fidget",
-            -- "flash",
-            -- "gitsigns",
-            -- "harpoon",
-            -- "hop",
-            -- "illuminate",
-            -- "indent-blankline",
-            -- "lspsaga",
-            -- "mini",
-            -- "neogit",
-            -- "neotest",
-            -- "neo-tree",
-            -- "neorg",
-            -- "noice",
-            -- "nvim-cmp",
-            -- "nvim-navic",
-            -- "nvim-tree",
-            -- "nvim-web-devicons",
-            -- "rainbow-delimiters",
-            -- "sneak",
-            -- "telescope",
-            -- "trouble",
-            -- "which-key",
-            -- "nvim-notify",
-        },
+    plugins = { -- Uncomment the plugins that you use to highlight them
+        -- Available plugins:
+        -- "coc",
+        -- "colorful-winsep",
+        -- "dap",
+        -- "dashboard",
+        -- "eyeliner",
+        -- "fidget",
+        -- "flash",
+        -- "gitsigns",
+        -- "harpoon",
+        -- "hop",
+        -- "illuminate",
+        -- "indent-blankline",
+        -- "lspsaga",
+        -- "mini",
+        -- "neogit",
+        -- "neotest",
+        -- "neo-tree",
+        -- "neorg",
+        -- "noice",
+        -- "nvim-cmp",
+        -- "nvim-navic",
+        -- "nvim-tree",
+        -- "nvim-web-devicons",
+        -- "rainbow-delimiters",
+        -- "sneak",
+        -- "telescope",
+        -- "trouble",
+        -- "which-key",
+        -- "nvim-notify",
+    },
 
-        disable = {
-            colored_cursor = false, -- Disable the colored cursor
-            borders = false, -- Disable borders between vertically split windows
-            background = false, -- Prevent the theme from setting the background (NeoVim then uses your terminal background)
-            term_colors = false, -- Prevent the theme from setting terminal colors
-            eob_lines = false -- Hide the end-of-buffer lines
-        },
+    disable = {
+        colored_cursor = false, -- Disable the colored cursor
+        borders = false, -- Disable borders between vertically split windows
+        background = false, -- Prevent the theme from setting the background (NeoVim then uses your terminal background)
+        term_colors = false, -- Prevent the theme from setting terminal colors
+        eob_lines = false -- Hide the end-of-buffer lines
+    },
 
-        high_visibility = {
-            lighter = false, -- Enable higher contrast text for lighter style
-            darker = false -- Enable higher contrast text for darker style
-        },
+    high_visibility = {
+        lighter = false, -- Enable higher contrast text for lighter style
+        darker = false -- Enable higher contrast text for darker style
+    },
 
-        lualine_style = "default", -- Lualine style ( can be 'stealth' or 'default' )
+    lualine_style = "stealth", -- Lualine style ( can be 'stealth' or 'default' )
 
-        async_loading = false, -- Load parts of the theme asynchronously for faster startup (turned on by default)
+    async_loading = false, -- Load parts of the theme asynchronously for faster startup (turned on by default)
 
-        custom_colors = nil, -- If you want to override the default colors, set this to a function
+    custom_colors = nil, -- If you want to override the default colors, set this to a function
 
-        custom_highlights = {} -- Overwrite highlights with your own
-    })
-    vim.g.material_style = 'deep ocean'
-    vim.cmd 'colorscheme material'
-end
+    custom_highlights = {} -- Overwrite highlights with your own
+}
 
-M.load_material()
-
-return M
+vim.g.material_style = 'deep ocean'
+vim.cmd 'colorscheme material'
