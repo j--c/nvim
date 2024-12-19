a38fbcd4c6c358f6b73d9c2aeaa0910c6b5e942e
j--c <johnmichaelcollins@protonmail.com>
2024-12-18 23:03:43 -0500

    updated lsp config key maps to make it easier to remember

--- after/plugin/lsp_config.lua
+++ after/plugin/lsp_config.lua
@@ -4,8 +4,6 @@ lsp_zero.on_attach(function(client, bufnr)
     local opts = {buffer = bufnr, remap = false}
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
     vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
