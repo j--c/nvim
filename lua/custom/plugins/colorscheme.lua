e91afefdfbe36ed27e4b66e7e9ad46f2587346e3
j--c <johnmichaelcollins@protonmail.com>
2025-10-13 18:31:52 -0400

    change from terafox to nightfox

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -20,6 +20,6 @@ return {
         lazy = false,
         priority = 1000,
         opts = {},
-        config = function() vim.cmd[[colorscheme terafox]] end
+        config = function() vim.cmd[[colorscheme nightfox]] end
     }
 }
