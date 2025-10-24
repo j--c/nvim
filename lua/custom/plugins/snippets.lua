f78a2a465fe6157e66c09b204d406b8c6d56dfaa
j--c <johnmichaelcollins@protonmail.com>
2025-10-24 10:33:22 -0400

    added python-snipppet dependency

--- lua/custom/plugins/snippets.lua
+++ lua/custom/plugins/snippets.lua
@@ -4,7 +4,7 @@
 return {
     {
         "L3MON4D3/LuaSnip",
-        dependencies = { "rafamadriz/friendly-snippets" },
+        dependencies = { "rafamadriz/friendly-snippets", "cstrap/python-snippets" },
         build = "make install_jsregexp",
         config = function()
             local ls = require("luasnip")
@@ -12,7 +12,6 @@ return {
             vim.keymap.set({"i"}, "<C-K>", function() ls.expand() end, {silent = true})
             vim.keymap.set({"i", "s"}, "<C-L>", function() ls.jump( 1) end, {silent = true})
             vim.keymap.set({"i", "s"}, "<C-J>", function() ls.jump(-1) end, {silent = true})
-
             vim.keymap.set({"i", "s"}, "<C-E>", function()
                 if ls.choice_active() then
                     ls.change_choice(1)
