c984fac5678808a2c49105670c065ffef2cf08d1
j--c <johnmichaelcollins@protonmail.com>
2025-10-30 22:36:57 -0400

    added completion options

--- lua/custom/plugins/completion.lua
+++ lua/custom/plugins/completion.lua
@@ -4,17 +4,17 @@
 return {
     {
         'saghen/blink.cmp',
-        -- optional: provides snippets for the snippet source
-        dependencies = { 'rafamadriz/friendly-snippets' },
-
-        -- use a release tag to download pre-built binaries
         version = '1.*',
         opts = {
             keymap = { preset = 'default' },
             appearance = {
                 nerd_font_variant = 'mono'
             },
+            sources = {
+                default = { 'lsp', 'path', 'snippets', 'buffer' },
+            },
             fuzzy = { implementation = "prefer_rust_with_warning" }
         },
+        opts_extend = {"sources.default"}
     }
 }
